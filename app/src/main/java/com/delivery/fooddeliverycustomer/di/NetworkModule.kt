ea4b4e7c6269d169698963db08bdb10af6be5d22
Biju Mondal <biju.m@op3n.world>
2026-08-21 10:38:30 +0530

    network module DI - done

--- app/src/main/java/com/delivery/fooddeliverycustomer/di/NetworkModule.kt
+++ app/src/main/java/com/delivery/fooddeliverycustomer/di/NetworkModule.kt
@@ -1,6 +1,12 @@
 package com.delivery.fooddeliverycustomer.di
 
+import com.delivery.fooddeliverycustomer.data.remote.api.AddressApi
+import com.delivery.fooddeliverycustomer.data.remote.api.AuthApi
+import com.delivery.fooddeliverycustomer.data.remote.api.CartApi
+import com.delivery.fooddeliverycustomer.data.remote.api.FavouriteApi
+import com.delivery.fooddeliverycustomer.data.remote.api.OrderApi
 import com.delivery.fooddeliverycustomer.data.remote.api.RestaurantApi
+import com.delivery.fooddeliverycustomer.domain.model.cart.Cart
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +33,18 @@ object NetworkModule {
             .build()
     }
 
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(
+        retrofit: Retrofit
+    ): AuthApi {
+
+        return retrofit.create(
+            AuthApi::class.java
+        )
+    }
+
     @Provides
     @Singleton
     fun provideRestaurantApi(
@@ -35,6 +53,51 @@ object NetworkModule {
 
         return retrofit.create(
             RestaurantApi::class.java
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddressApi(
+        retrofit: Retrofit
+    ): AddressApi {
+
+        return retrofit.create(
+            AddressApi::class.java
+        )
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideCartApi(
+        retrofit: Retrofit
+    ): CartApi {
+
+        return retrofit.create(
+            CartApi::class.java
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideOrderApi(
+        retrofit: Retrofit
+    ): OrderApi {
+
+        return retrofit.create(
+            OrderApi::class.java
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideFavouriteApi(
+        retrofit: Retrofit
+    ): FavouriteApi {
+
+        return retrofit.create(
+            FavouriteApi::class.java
         )
     }
 }
